32a3b728a36b2c968935e2354917927ccedc8c65
yangyu <648821159@qq.com>
2020-09-17 11:27:47 +0800

    新增orm连接池的配置

--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -15,6 +15,8 @@ use EasySwoole\Http\Request;
 use EasySwoole\Http\Response;
 use App\Producer\Process as ProducerProcess;
 use App\Consumer\Process as ConsumerProcess;
+use EasySwoole\ORM\DbManager;
+use EasySwoole\ORM\Db\Connection;
 
 class EasySwooleEvent implements Event
 {
@@ -23,6 +25,15 @@ class EasySwooleEvent implements Event
     {
         // TODO: Implement initialize() method.
         date_default_timezone_set('Asia/Shanghai');
+        $config = new \EasySwoole\ORM\Db\Config(Config::getInstance()->getConf('MYSQL'));
+        //连接池配置
+        $config->setGetObjectTimeout(3.0); //设置获取连接池对象超时时间
+        $config->setIntervalCheckTime(30*1000); //设置检测连接存活执行回收和创建的周期
+        $config->setMaxIdleTime(15); //连接池对象最大闲置时间(秒)
+        $config->setMaxObjectNum(20); //设置最大连接池存在连接对象数量
+        $config->setMinObjectNum(5); //设置最小连接池存在连接对象数量
+        $config->setAutoPing(5); //设置自动ping客户端链接的间隔
+        DbManager::getInstance()->addConnection(new Connection($config));
     }
 
     public static function mainServerCreate(EventRegister $register)
